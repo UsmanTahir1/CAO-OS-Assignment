a22ed8cde45958bdd7264486efd382d23d3652d7
UsmanTahir1 <82867460+UsmanTahir1@users.noreply.github.com>
2021-04-20 12:26:38 +0500

    Add files via upload

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,12 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+	print_str("*************        *        *        ********      *********\n");
+	print_str("*           *        *        *        *                 *\n");
+	print_str("*           *        *        *        *                 *\n");
+	print_str("*           *        *        *        ********          *\n");
+	print_str("*           *        *        *               *          *\n");
+	print_str("*           *        *        *               *          *\n");
+	print_str("*           *        *********        ********           *\n");
+}
